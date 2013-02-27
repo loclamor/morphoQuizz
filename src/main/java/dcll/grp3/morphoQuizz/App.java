edad416877188273573801deedbc2dec3ca6a1db
loclamor <favier.loic.31@gmail.com>
2013-02-27 15:22:18 +0100

    #7 creation du projet maven

---
+++ src/main/java/dcll/grp3/morphoQuizz/App.java
@@ -0,0 +1,13 @@
+package dcll.grp3.morphoQuizz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
