ce8c0bc1466f211b4f945bb66eacee7711ee97d2
Guillaume <guillaume.jandin@gmail.com>
2013-03-10 20:00:20 +0100

    ajout/correction tests quizConverter #4

--- src/test/java/dcll/grp3/morphoQuizz/QuizzConverterTest.java
+++ src/test/java/dcll/grp3/morphoQuizz/QuizzConverterTest.java
@@ -9,31 +9,76 @@ import junit.framework.TestSuite;
 
 public class QuizzConverterTest extends TestCase {
 	
-	public QuizzConverterTest ( String testName ) 
+	protected String[] filesNames = { "quiz-moodle-exemple.xml", "quiz-moodle-exemple.json" };
+	protected File testFolder, testInput, testOutput;
+	protected String ext;
+	
+	public QuizzConverterTest (String testName) 
 	{
-		super( testName );
+		super(testName);
 	}
 	
 	public static Test suite()
     {
-        return new TestSuite( QuizzConverterTest.class );
+        return new TestSuite(QuizzConverterTest.class);
     }
 	
 	 public void testQuizzConverter()
 	 {
-		 assertTrue( true );
+		 assertTrue(true);
+	 }
+	 
+	 public void setUp() throws Exception {
+		 super.setUp();
+		 System.out.println("SetUp");
+
+		 //creation du dossier data
+		 testFolder = new File("data");
+		 testFolder.mkdir();
+		 
+		 //lecture des fichiers dans data
+		 for (int i=0; i<filesNames.length; i++) {
+			 testInput = new File(testFolder.getPath() + "/" + filesNames[i]);
+			 if (testInput.getName().lastIndexOf(".") > 0) {
+				 ext = testInput.getName().substring(testInput.getName().lastIndexOf("."));
+				 //traitement selon l'extension
+				 if (ext.equals(".xml")) {
+					 testOutput = new File(testFolder.getPath() + "/" + filesNames[i].substring(0,filesNames[i].indexOf(".")) + ".json");
+					 QuizzConverter.XMLQuizzToJson(testInput, testOutput);
+					 assertTrue(testOutput.exists());
+				 }
+				 else if (ext.equals(".json")) {
+					 testOutput = new File(testFolder.getPath() + "/" + filesNames[i].substring(0,filesNames[i].indexOf(".")) + ".xml");
+					 QuizzConverter.JsonQuizzToXML(testInput, testOutput);
+					 assertTrue(testOutput.exists());
+				 }
+				 else {
+					 System.out.println("Aucun fichier a convertir n'est present ! (.xml ou .json)");
+				 }
+			 }
+		 }
+		 
 	 }
 
-	public static void testXMLQuizzToJson (File xmlInput, File jsonOutput)
+	 public void tearDown() throws Exception {
+		 super.tearDown();
+		 System.out.println("TearDown");
+		 //suppression des fichiers de tests
+		 for (int i=0; i<filesNames.length; i++) {
+			 File file = new File(testFolder.getPath() + "/" + filesNames[i]);
+			 file.delete();
+		 }
+		 testFolder.delete();
+	 }
+	 
+	public void testXMLQuizzToJson ()
 			throws IOException {
-		assertNotNull(xmlInput);
-		assertNotNull(jsonOutput);
+		System.out.println("testXMLQuizzToJson");
 	}
 	
-	public static void testJsonQuizzToXML (File jsonInput, File xmlOutput)
+	public void testJsonQuizzToXML ()
 			throws IOException {
-		assertNotNull(jsonInput);
-		assertNotNull(xmlOutput);
+		System.out.println("testJsonQuizzToXML");
 	}
 	
 	
