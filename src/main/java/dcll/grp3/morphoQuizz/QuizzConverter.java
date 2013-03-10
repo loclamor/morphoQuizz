6411073a7325565eb687623f3ce91d36dfd1e70b
legaliz_me <r3m1.benoit@gmail.com>
2013-03-10 15:05:52 +0100

    Ajout du squelette de la classe de conversion des quizz

---
+++ src/main/java/dcll/grp3/morphoQuizz/QuizzConverter.java
@@ -0,0 +1,17 @@
+package dcll.grp3.morphoQuizz;
+
+import java.io.File;
+import java.io.IOException;
+
+public class QuizzConverter {
+
+	public static void XMLQuizzToJson(File xmlInput, File jsonOutput)
+			throws IOException {
+	}
+
+	public static void JsonQuizzToXML(File xmlInput, File jsonOutput)
+			throws IOException {
+
+	}
+
+}
