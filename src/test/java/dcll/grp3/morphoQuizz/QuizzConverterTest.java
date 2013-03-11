cc4d8a149d7cd369d02e8aafe415df132cbceedb
Guillaume <guillaume.jandin@gmail.com>
2013-03-11 18:14:30 +0100

    ajout comparaison 2 xml #4

--- src/test/java/dcll/grp3/morphoQuizz/QuizzConverterTest.java
+++ src/test/java/dcll/grp3/morphoQuizz/QuizzConverterTest.java
@@ -1,14 +1,19 @@
 package dcll.grp3.morphoQuizz;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.custommonkey.xmlunit.XMLTestCase;
+
 import junit.framework.Test;
-import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class QuizzConverterTest extends TestCase {
+public class QuizzConverterTest extends XMLTestCase {
 	
 	protected String[] filesNames = { "quiz-moodle-exemple.xml", "quiz-moodle-exemple.json" };
-	protected File testFolder, testInput, testOutput;
+	protected File testFolder, testInput, testOutput, testOutput2;
 	protected String ext;
 	
 	public QuizzConverterTest (String testName) 
@@ -54,15 +59,25 @@ public class QuizzConverterTest extends TestCase {
 		 //selection des fichiers xml dans data tmp
 		 for (int i=0; i<filesNames.length; i++) {
 			 testInput = new File("data/" + filesNames[i]);
+			 testOutput = new File(testFolder.getPath() + "/" + filesNames[i].substring(0,filesNames[i].indexOf(".")) + ".json");
+			 testOutput2 = new File(testFolder.getPath() + "/" + filesNames[i].substring(0,filesNames[i].indexOf(".")) + "2.xml");
+			 InputStream original = new FileInputStream(testInput);
+			 InputStream converti = new FileInputStream(testOutput2);
 			 if (testInput.getName().lastIndexOf(".") > 0) {
 				 ext = testInput.getName().substring(testInput.getName().lastIndexOf("."));
 				 //traitement selon l'extension
 				 if (ext.equals(".xml")) {
-					 testOutput = new File(testFolder.getPath() + "/" + filesNames[i].substring(0,filesNames[i].indexOf(".")) + ".json");
 					 QuizzConverter.xmlQuizzToJson(testInput, testOutput);
 					 assertTrue(testOutput.exists());
 				 }
 				 //verifie l'egalite entre 2 fichiers
+				 if (ext.equals(".json")) {
+					 QuizzConverter.jsonQuizzToXML(testOutput, testOutput2);
+					 String myControlXML = "<msg><uuid>0x00435A8C</uuid></msg>";
+					 String myTestXML = "<msg><uuid>0x00435A8C</uuid></msg>";
+					 assertXMLEqual(myControlXML, myTestXML);
+				 }
+				 assertXMLEqual(IOUtils.toString(original), IOUtils.toString(converti));
 			 }
 		 }
 	}
