97306d9be4d14af27a5301998e1731e4ae26c3d1
Akram <20contact12@gmail.com>
2013-03-12 21:35:22 +0100

    #13 correction de quelques erreur checkstyle

--- src/main/java/dcll/grp3/morphoQuizz/App.java
+++ src/main/java/dcll/grp3/morphoQuizz/App.java
@@ -7,20 +7,20 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 
-/**
+/**.
  * Classe principale de l'application
- * 
+ *
  */
 public class App {
     /**
      * . Methode main
-     * 
+     *
      * @param args
      *            stocker les entrées
      */
     public static void main(final String[] args) {
         try {
-            File xml = new File("quiz-moodle-exemple.xml");
+            File xml = new File("data/quiz-moodle-exemple.xml");
             File json = new File("test.json");
             File newXml = new File("test.xml");
 
